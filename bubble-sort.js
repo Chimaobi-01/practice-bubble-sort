a72a155b71bf39d49d43ecced5a367a653221b4a
Chinaka_Chimaobi <80992410+Chimaobi-01@users.noreply.github.com>
2025-09-03 07:05:06 +0000

    sorted

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,18 +1,30 @@
 
 function bubbleSort(arr) {
 
+  let pass = true;
+
+  while (pass) {
+    pass = false;
+
     // Iterate through the array
+    for (let i = 0; i < arr.length - 1; i++) {
+      const currentValue = arr[i];
+      const neighbor = arr[i + 1];
 
       // If the current value is greater than its neighbor to the right
-        // Swap those values
-
+      // Swap those values
+      if (currentValue > neighbor) {
+        [arr[i], arr[i + 1]] = [neighbor, currentValue];
+        pass = true;
         // Do not move this console.log
         console.log(arr.join(","));
+      }
 
+    }
     // If you get to the end of the array and no swaps have occurred, return
 
     // Otherwise, repeat from the beginning
-
+  }
 }
 
 module.exports = bubbleSort;
